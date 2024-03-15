6abe36d1cf7b68731d8ee6b420fd2aab17e81850
Naomie <na.gasnault@gmail.com>
2024-03-15 16:47:04 +0100

    ajout html, css, js,  responsive

---
+++ main.js
@@ -0,0 +1,15 @@
+
+ 
+
+
+
+
+var typed = new Typed(".interactif-text", {
+    strings: ["Conceptrice Développeuse d'Applications", "Curieuse", "Disponible dès maintenant en alternance !"],
+    typeSpeed: 65,
+    backSpeed: 10,
+    backDelay: 1000,
+    loop: true
+  });
+  
+
